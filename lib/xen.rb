f4dec38044dbe82a3314349860d189aba93fca2a
Nico <nico@rottenbytes.info>
2010-04-06 09:11:30 +0200

    First commit - very basic functionnality
    Signed-off-by: Nico <nico@rottenbytes.info>

---
+++ lib/xen.rb
@@ -0,0 +1,76 @@
+module Xen
+    class XenInstance
+        attr_accessor :name, :memory, :id, :vcpus, :state, :time
+
+	    def initialize(name, options=())
+		    @name=name
+		    @memory=options[:memory]
+		    @id=options[:id]
+		    @vcpus=options[:vcpus]
+		    @state=options[:state]
+		    @time=options[:time]
+	    end
+
+	    def running?
+		    output=`xm list #{@name}`
+		    $? == 0 ? true : false
+	    end        
+    end
+
+
+    class XenServer
+	    def initialize
+		    @domus = {}
+		    output = `xm list`
+		
+		    output.each { |line|
+			    line.grep(/(.*)\s+(\d+)\s+(\d+)\s+(\d+)\s+(.*?)\s+(\d+.\d)/) {
+			        @domus[$1.strip] = XenInstance.new($1.strip, 
+                                :id => $2.strip,
+                                :memory => $3.strip,
+                                :vcpus => $4.strip,
+                                :state => $5.strip,
+                                :time => $6.strip )
+                }
+		    }
+	    end
+
+	    def update 
+		    output = `xm list`
+		
+		    output.each { |line|
+			    line.grep(/(.*)\s+(\d+)\s+(\d+)\s+(\d+)\s+(.*?)\s+(\d+.\d)/) {
+			        @domus[$1.strip].id = $2.strip
+                    @domus[$1.strip].memory = $3.strip
+                    @domus[$1.strip].vcpus = $4.strip
+                    @domus[$1.strip].state = $5.strip
+                    @domus[$1.strip].time = $6.strip
+                }
+		    }
+	    end
+	
+	    def slices
+	        rslt = []
+	        @domus.each_key { |k|
+	            rslt << k
+	        }
+	        rslt
+	    end
+	
+	    def has?(name)
+	        if @domus.has_key? name then
+	            true
+	        else
+	            false
+	        end
+	    end
+	
+	    def get(name)
+	        if self.has? name then
+	            @domus[name]
+	        else
+	            nil
+            end
+        end
+    end
+end
---
+++ xen.gemspec
@@ -0,0 +1,14 @@
+Gem::Specification.new do |s|
+  s.name = "xen"
+  s.version = "0.0.1"
+  s.date = "2010-04-06"
+  s.authors = ["nico"]
+  s.email = "nico@rottenbytes.info"
+  s.rubyforge_project = "xen"
+  s.has_rdoc = false
+  s.summary = "Library for accessing Xen informations"
+  s.homepage = "http://www.github.com/rottenbytes/ruby-xen"
+  s.description = ""
+  s.files = ["README",
+             "lib/xen.rb"]
+end
